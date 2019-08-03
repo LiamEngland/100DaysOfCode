01459601f5b3777cc5f823172c96be4dbc6833f2
Liam England <31520387+LiamEngland@users.noreply.github.com>
2019-08-03 21:44:21 +0100

    Completed day 19

--- Media App/header.php
+++ Media App/header.php
@@ -16,13 +16,14 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <title><?= $title ?></title>
     <link rel='stylesheet' href='assets/css/style.css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-0c38nfCMzF8w8DBI+9nTWzApOpr1z0WuyswL4y6x/2ZTtmj/Ki5TedKeUcFusC/k" crossorigin="anonymous">
 </head>
 <body>
 <div class='wrapper'>
 <nav class='navbar sticky-top navbar-expand-lg navigation'>
-    <a class='navbar-brand' href='home.php' active><b>MEDIA APP</b></a>
+    <a class='navbar-brand' href='home.php' active>MEDIA APP</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -32,7 +33,10 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                 <a class="nav-link" href='home.php'>HOME <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">LIBRARY <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href='library.php'>LIBRARY <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href='videos.php'>VIDEOS <span class="sr-only">(current)</span></a>
             </li>
         </ul>
         <ul class="navbar-nav">
---
+++ Media App/videos.php
@@ -0,0 +1,22 @@
+<?php
+include_once('config.php');
+?>
+
+<?php
+include_once('header.php');
+?>
+
+<div class='container-fluid mb-2'>
+    <div class='row'>
+        <div class='col-8 offset-2'>
+            <video width="100%" controls class='mt-5'>
+                <source src="uploads/video/4.mp4" type="video/mp4">
+                Your browser isn't supported.
+            </video>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once('footer.php');
+?>
