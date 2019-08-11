911aa33041ed3fa185d84ef979142533a2148782
Liam England <31520387+LiamEngland@users.noreply.github.com>
2019-08-11 20:15:42 +0100

    competed day 27

--- Media App/playfooter.php
+++ Media App/playfooter.php
@@ -1,11 +1,21 @@
 <div class="push"></div>
 </div>
 <footer class="footer text-center">
-    <audio controls>
-        <source src="uploads/audio/1.mp3" type="audio/mpeg">
-        Your browser isn't supported.
-    </audio>
+    <div class='container-fluid h-100'>
+        <div class='row align-items-center h-100'>
+            <div class='col-sm-12 col-md-6'>
+                <span class='backButton'><i class="fas fa-backward"></i></span>
+                <span class='playButton'><i class="far fa-play-circle"></i></span>
+                <span class='forwardButton'><i class="fas fa-forward"></i></span>
+            </div>
+            <div class='col-sm-12 col-md-6'>
+                <span class='float-left'>0:00 / 1:00</span>
+                <span>Song Name Here</span>
+            </div>
+        </div>
+    </div>
 </footer>
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
