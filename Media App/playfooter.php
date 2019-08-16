dcdc6ddb0458ccaaa19e5e196a7e1444bb191370
Liam England <31520387+LiamEngland@users.noreply.github.com>
2019-08-16 20:58:50 +0100

    day 32 complete

--- Media App/playfooter.php
+++ Media App/playfooter.php
@@ -1,9 +1,7 @@
 <div class="push"></div>
 </div>
 
-<audio id='playFooter' src='uploads/audio/1.mp3'>
-
-</audio>
+<audio id='playFooter' src='uploads/audio/1.mp3'></audio>
 
 <footer class="footer text-center">
     <div class='container-fluid h-100'>
@@ -26,6 +24,69 @@
         </div>
     </div>
 </footer>
+<script type='text/javascript'>
+    var songs = [];
+    var songTitle = document.getElementById("songTitle");
+    var fillBar = document.getElementById("fill");
+
+    var song = new Audio();
+    var currentSong = 0;    // it point to the current song
+
+    window.onload = playSong;   // it will call the function playSong when window is load
+
+    function playSong(){
+        
+        song.src = songs[currentSong];  //set the source of 0th song 
+        
+        songTitle.textContent = songs[currentSong]; // set the title of song
+        
+        song.play();    // play the song
+    }
+
+    function playOrPauseSong(){
+        
+        if(song.paused){
+            song.play();
+            $("#play img").attr("src","Pause.png");
+        }
+        else{
+            song.pause();
+            $("#play img").attr("src","Play.png");
+        }
+    }
+
+    song.addEventListener('timeupdate',function(){ 
+        
+        var position = song.currentTime / song.duration;
+        
+        fillBar.style.width = position * 100 +'%';
+    });
+
+
+    function next(){
+        
+        currentSong++;
+        if(currentSong > 2){
+            currentSong = 0;
+        }
+        playSong();
+        $("#play img").attr("src","Pause.png");
+        $("#image img").attr("src",poster[currentSong]);
+        $("#bg img").attr("src",poster[currentSong]);
+    }
+
+    function pre(){
+        
+        currentSong--;
+        if(currentSong < 0){
+            currentSong = 2;
+        }
+        playSong();
+        $("#play img").attr("src","Pause.png");
+        $("#image img").attr("src",poster[currentSong]);
+        $("#bg img").attr("src",poster[currentSong]);
+    }
+</script>
 <script src='assets/js/audioFile.js'></script>
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
